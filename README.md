8c00eb2f05e514c9781b1333055e1189f122e40b
Sergey Gospodarchuk <69005134+gosxrgxx@users.noreply.github.com>
2022-08-31 02:33:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# DOTFILES
+
+## Here are some details:
+- **OS**: `Arch Linux`
+- **WM**: `bspwm`
+- **Terminal**: `kitty`
+- **Bar**: `polybar`
+- **Launcher**: `rofi`, `networkmanager_dmenu`
+- **Theme**: [`gruvbox GTK Theme`](https://www.gnome-look.org/p/1681313)
+- **Icon**: [`colloid icon theme`](https://github.com/vinceliuice/Colloid-icon-theme)
+- **Cursor**: [`simp1e Cursors`](https://www.gnome-look.org/p/1405210)
+- **Font**: [`JetBrains Mono Nerd Fonts`](https://github.com/ryanoasis/nerd-fonts)
+
+## Source:
+- [colors](https://github.com/morhetz/gruvbox#palette)
+- [adi1090x/hack](https://github.com/adi1090x/polybar-themes#hack)
---
+++ dot_oh-my-zsh/custom/alias.zsh
@@ -0,0 +1,30 @@
+# Default
+# ---
+alias ls='ls -l --color=auto'
+alias lsa='ls -la --color=auto'
+alias c='clear'
+alias h='history'
+alias open='xdg-open'
+alias erase='shred -uvz'
+
+# Pacman
+# ---
+alias p-='sudo pacman'
+alias p-s='sudo pacman -S'
+alias p-ss='sudo pacman -Ss'
+alias p-si='sudo pacman -Si'
+alias p-u='sudo pacman -Syu'
+alias p-qs='sudo pacman -Qs'
+alias p-rsn='sudo pacman -Rsn'
+
+# Fast edit
+# ---
+alias e-xinitrc='sudo subl ~/.xinitrc'
+alias e-bspwm='chezmoi edit ~/.config/bspwm/bspwmrc'
+alias e-sxhkd='chezmoi edit ~/.config/sxhkd/sxhkdrc'
+alias e-alias='chezmoi edit ~/.oh-my-zsh/custom/alias.zsh'
+alias e-picom='chezmoi edit ~/.config/picom.conf'
+alias e-polybar='chezmoi edit ~/.config/polybar/config.ini'
+
+# Git
+# ---
